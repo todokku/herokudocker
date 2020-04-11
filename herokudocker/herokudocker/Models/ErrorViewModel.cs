0e77b7e40ed0d7e59830b94f6b31e3c1dfde7561
Sandor Lorinc <slorinc@logmein.com>
2020-04-11 19:22:29 +0200

    up

---
+++ herokudocker/herokudocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace herokudocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
