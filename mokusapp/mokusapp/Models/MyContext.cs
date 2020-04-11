e575b460a770de905a182da3134cd75562fa01cd
Sandor Lorinc <slorinc@logmein.com>
2020-04-11 21:25:15 +0200

    asd

--- mokusapp/mokusapp/Models/MyContext.cs
+++ mokusapp/mokusapp/Models/MyContext.cs
@@ -8,8 +8,16 @@ namespace herokudocker.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(@"Host=ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432;"
-                + "Database=d4b3kmir1pe427;Username=slppjedhxbbwnk;Password=d7642369b5226d43115e5e79caf1b1d86656eae9d72e1577795e8678db055280");
+            var host = "ec2-46-137-84-173.eu-west-1.compute.amazonaws.com";
+            var port = 5432;
+            var databaseName = "d4b3kmir1pe427";
+            var userName = "slppjedhxbbwnk";
+            var password = "d7642369b5226d43115e5e79caf1b1d86656eae9d72e1577795e8678db055280";
+
+            var connString = $"Server={host}; " + $"Port={port}; " +
+                $"User Id={userName};" + $"Password={password};" + $"Database={databaseName};";
+
+            optionsBuilder.UseNpgsql(connString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
